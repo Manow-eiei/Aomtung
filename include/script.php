8fc66e98ceee6dd74e2ba009d14de190beb423ec
ManowEiEi <nowma061@gmail.com>
2024-02-14 09:28:45 +0700

    login

---
+++ include/script.php
@@ -0,0 +1,199 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+<script src="https://kit.fontawesome.com/46d84e83e2.js" crossorigin="anonymous"></script>
+<script src="includes/jquery-3.6.4.js"></script>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<style>
+/* .bg-user{
+        position: absolute;
+        width: 950px;
+        height: 970px;
+        left: 950px;
+        top: 0px;
+
+        background: linear-gradient(152.26deg, #F3558B 0%, #89163D 95.36%);
+        } */
+
+
+.img {
+    border-radius: 100%;
+    width: 30px;
+
+}
+
+.btn-pink-user {
+    background: linear-gradient(90deg, #F3558B 33.4%, #89163D 96.4%);
+    color: white;
+}
+
+.bg-pink {
+    background: #F3558B;
+}
+
+.bg-dark-blue {
+    position: absolute;
+    width: 950px;
+    height: 970px;
+    left: 950px;
+    top: 0px;
+    background: linear-gradient(155.11deg, #960E3D 0.74%, #351291 97.38%);
+}
+
+.btn-pink {
+    background: #F3558B;
+    border-radius: 100px;
+    color: white;
+}
+
+.btn-pink2 {
+    /* background: #F3558B; */
+    background: linear-gradient(90deg, #F3558B 12.77%, #F3558B 72.27%);
+    color: white;
+    
+}
+
+.btn-blue {
+    background: linear-gradient(90deg, #389AE9 12.77%, #389AE9 72.27%);
+    color: white;
+}
+
+.btn-pink-solid {
+    border: 1px solid #F3558B;
+    border-radius: 100px;
+
+}
+
+.btn-pink-blue {
+    background: linear-gradient(90deg, #F3558B 33.4%, #203864 96.4%);
+    color: white;
+}
+
+.btn-pink-purple {
+    background: linear-gradient(90deg, #351291 12.77%, #F3558B 72.27%);
+    color: white;
+}
+
+.text-pink {
+    color: #F3558B;
+}
+
+.text-line {
+    color: #06C755;
+}
+
+
+.text-purple {
+    color: #8E30BF;
+}
+
+.text-blue {
+    color: #389AE9;
+}
+
+.btn-purple {
+    background: linear-gradient(90deg, #2d0df0b3 33.4%, #2d0df0b3 96.4%);
+    color: white;
+}
+
+
+.gradient-custom-1 {
+
+    /* Chrome 10-25, Safari 5.1-6 */
+    background: -webkit-linear-gradient(155.11deg, #F3558B 0.74%, #351291 97.38%);
+
+    /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+    background: linear-gradient(155.11deg, #F3558B 0.74%, #351291 97.38%);
+}
+
+@media (min-width: 768px) {
+    .gradient-form {
+        height: 100vh !important;
+    }
+}
+
+@media (min-width: 769px) {
+    .gradient-custom-2 {
+        border-top-right-radius: .3rem;
+        border-bottom-right-radius: .3rem;
+    }
+}
+
+.gradient-custom-2 {
+
+    /* Chrome 10-25, Safari 5.1-6 */
+    background: -webkit-linear-gradient(152.26deg, #F3558B 0%, #89163D 95.36%);
+
+    /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+    background: linear-gradient(152.26deg, #F3558B 0%, #89163D 95.36%);
+}
+
+@media (min-width: 768px) {
+    .gradient-form {
+        height: 100vh !important;
+    }
+}
+
+@media (min-width: 769px) {
+    .gradient-custom-2 {
+        border-top-right-radius: .3rem;
+        border-bottom-right-radius: .3rem;
+    }
+}
+
+.onmouseover :hover {
+    background-color: rgb(245, 245, 245);
+}
+
+
+*,
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+
+{
+    font-family: 'Prompt', sans-serif;
+}
+
+.small-h6 {
+    font-size: 14px;
+    font-family: 'Prompt', sans-serif;
+}
+
+.small-h5 {
+    font-size: 17px;
+    font-family: 'Prompt', sans-serif;
+}
+
+/* Make the image fully responsive */
+.carousel-inner img {
+    width: 100%;
+    height: 100%;
+}
+
+.no-underline {
+    text-decoration: none; /* ลบเส้นใต้ */
+}
+
+
+</style>
+
+<!-- font Prompt from google -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Prompt&display=swap" rel="stylesheet">
+
+<!-- carousel -->
+<link href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" rel="stylesheet">
+<link href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css"
+    rel="stylesheet">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+<script src="carouse.js"></script>
+<link rel="stylesheet" href="carouse.css">
+
