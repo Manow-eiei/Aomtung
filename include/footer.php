6c35b78b39bf240b0d363152701377effd954edc
ManowEiEi <nowma061@gmail.com>
2024-02-14 13:00:58 +0700

    ล่าสุด

---
+++ include/footer.php
@@ -0,0 +1,17 @@
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+    integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
+</script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"
+    integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous">
+</script>
+
+<!-- Footer -->
+<footer class="sticky-footer bg-white">
+    <div class="container my-auto">
+        <div class="copyright text-center my-auto">
+        <span>Copyright &copy; Aomtung Team 2024</span>
+        </div>
+    </div>
+</footer>
+<!-- End of Footer -->
---
+++ login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Aomtung</title>
+    <?php
+    include("include/script.php")
+    ?>
+
+</head>
+<body>
+<center>
+<br><br>
+<div class="container-fluid" style="text-align: center;">
+    <img src="img/logo.png" alt="Logo" width="350">
+    <br><br>
+    <h4>ระบุหมายเลขโทรศัพท์</h4>
+    <center><input type="" class="form-control" id="number" name="number" placeholder="063-xxxxxxx" style="width: 200px;"></center>
+
+</div>
+   
+</center>
+    
+
+   
+</body>
+</html>
